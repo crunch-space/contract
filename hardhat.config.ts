c14679c62cfdbc30b593cd614660203ba21b3b70
yanghao <yanghao@ohdat.io>
2024-04-30 07:19:01 +0800

    Add environment variables and network configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,77 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [String(process.env.PRIVATE_KEY)],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [String(process.env.PRIVATE_KEY)],
+    },
+    // for mainnet
+    "base-mainnet": {
+      url: "https://mainnet.base.org",
+      accounts: [String(process.env.PRIVATE_KEY)],
+    },
+    // for testnet
+    "base-sepolia": {
+      url: "https://sepolia.base.org",
+      accounts: [String(process.env.PRIVATE_KEY)],
+    },
+    // for Sepolia testnet
+    blast_sepolia: {
+      url: "https://sepolia.blast.io",
+      accounts: [String(process.env.PRIVATE_KEY)],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: {
+      mainnet: `${process.env.ETHERSCAN_API_KEY}`, // process.env.ETHERSCAN_API_KEY,
+      goerli: `${process.env.ETHERSCAN_API_KEY}`,
+      blast_sepolia: "blast_sepolia", // apiKey is not required, just set a placeholder
+      "base-sepolia": `${process.env.BASE_ETHERSCAN_API_KEY}`,
+      "base-mainnet": `${process.env.BASE_ETHERSCAN_API_KEY}`,
+    },
+    customChains: [
+      {
+        network: "base-mainnet",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org",
+        },
+      },
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
+        },
+      },
+      {
+        network: "blast_sepolia",
+        chainId: 168587773,
+        urls: {
+          apiURL:
+            "https://api.routescan.io/v2/network/testnet/evm/168587773/etherscan",
+          browserURL: "https://testnet.blastscan.io",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: false,
+  },
 };
-
 export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,26 @@
+import { ethers, run } from "hardhat";
+
+async function main() {
+  const params = ["0x1D3f1A700b1e6df89E9Ef56689B6Ad4B5b932A88"];
+  // const c = await ethers.deployContract("CrunchProtocol", [
+  //   "0x1D3f1A700b1e6df89E9Ef56689B6Ad4B5b932A88",
+  // ]);
+  // const caddress = await c.getAddress();
+  const caddress = `0x52806ae50D45A2c9f1836eCA255E3A5b2108ecC4`;
+  console.log(`deployed to ${caddress}`);
+  //Delay a little
+  // await new Promise((resolve) => setTimeout(resolve, 20000));
+  // Verify the contract
+  await run("verify:verify", {
+    address: caddress,
+    constructorArguments: params,
+    contract: "contracts/CrunchProtocal.sol:CrunchProtocol",
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
